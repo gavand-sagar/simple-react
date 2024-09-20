fb4bbdceec08cc073b2a69d8eda863c0f2c06e0d
gavand-sagar <gavand.sagar.s@gmail.com>
2024-09-20 20:47:51 +0530

    simple api app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [categories, setCategories] = useState([]);
+  const [categoryName, setCategoryName] = useState('')
+
+  useEffect(() => {
+
+    axios.get("https://my-server-master-session-sep.onrender.com/get-all-categories")
+      .then(response => {
+        setCategories(response.data)
+      })
+
+  }, [])
+
+
+  function submit() {
+    let body = {
+      categoryName: categoryName
+    }
+    axios.post("https://my-server-master-session-sep.onrender.com/create-category",body)
+      .then(response => {
+        setCategories([...categories, body])
+        alert("Category Added")
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' value={categoryName} onChange={e => setCategoryName(e.target.value)} />
+      <button onClick={submit}>Add</button>
+      <h2>List of Categories</h2>
+      {
+        categories.map(x => <p>{x.categoryName}</p>)
+      }
     </div>
   );
 }
